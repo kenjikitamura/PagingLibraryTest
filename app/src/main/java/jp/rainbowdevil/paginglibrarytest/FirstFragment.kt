ed20d9c2c3d443d6426cba7552a5f5233ff06200
Kenji Kitamura <kenji@rainbowdevil.jp>
2020-10-16 23:18:51 +0900

    ダミーデータでリスト表示完了

--- app/src/main/java/jp/rainbowdevil/paginglibrarytest/FirstFragment.kt
+++ app/src/main/java/jp/rainbowdevil/paginglibrarytest/FirstFragment.kt
@@ -1,12 +1,19 @@
 package jp.rainbowdevil.paginglibrarytest
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import jp.rainbowdevil.paginglibrarytest.repository.FirstViewModel
 import org.koin.android.viewmodel.compat.ViewModelCompat
 
@@ -14,6 +21,8 @@ import org.koin.android.viewmodel.compat.ViewModelCompat
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    lateinit var adapter : TestItemAdapter
 
     private val viewModel : FirstViewModel by ViewModelCompat.viewModel(
         this,
@@ -24,7 +33,13 @@ class FirstFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        adapter = TestItemAdapter(requireContext())
         val binding = jp.rainbowdevil.paginglibrarytest.databinding.FragmentFirstBinding.inflate(layoutInflater)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = adapter
+        viewModel.list.observe(viewLifecycleOwner, Observer {
+            adapter.submitList(it)
+        })
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -32,4 +47,30 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
+    private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean = oldItem == newItem
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean =
+            oldItem.hashCode() == newItem.hashCode()
+    }
+
+    class TestItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView = itemView.findViewById<TextView>(R.id.text)
+    }
+
+    inner class TestItemAdapter(context: Context) : PagedListAdapter<String, TestItemViewHolder>(DIFF_CALLBACK) {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestItemViewHolder {
+            val inflater = LayoutInflater.from(context)
+            return TestItemViewHolder(inflater.inflate(R.layout.item, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: TestItemViewHolder, position: Int) {
+            holder.textView.text = getItem(position)
+        }
+    }
+
 }
+
+
+
+
--- app/src/main/java/jp/rainbowdevil/paginglibrarytest/repository/FirstViewModel.kt
+++ app/src/main/java/jp/rainbowdevil/paginglibrarytest/repository/FirstViewModel.kt
@@ -1,9 +1,44 @@
 package jp.rainbowdevil.paginglibrarytest.repository
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.*
 
 class FirstViewModel(
     val githubRepository: GithubRepository
 ) : ViewModel() {
 
+    private val factory :DataSource.Factory<Int, String> = TestDataSourceFactory()
+    val list: LiveData<PagedList<String>> = factory.toLiveData(50, 0)
+}
+
+class TestDataSource() : PageKeyedDataSource<Int, String>() {
+    override fun loadInitial(
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<Int, String>
+    ) {
+        callback.onResult(createDate(0), null, 1)
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, String>) {
+        callback.onResult(createDate(params.key), params.key + 1)
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, String>) {
+        callback.onResult(createDate(params.key), params.key + 1)
+    }
+
+    fun createDate(page: Int): List<String> {
+        val list = mutableListOf<String>()
+        for (i in 0..10) {
+            list.add("$page data ($i)")
+        }
+        return list
+    }
+}
+
+class TestDataSourceFactory() : DataSource.Factory<Int, String>() {
+    override fun create(): DataSource<Int, String> {
+        return TestDataSource()
+    }
 }
